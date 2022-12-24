d926ba996027eacd48b2ac6083ff58b5963f64ee
Akhil Kumar <46084762+akillkumar@users.noreply.github.com>
2022-12-24 17:45:14 +0530

    Update app.js

--- nodge/app.js
+++ nodge/app.js
@@ -11,12 +11,13 @@ var usersRouter = require("./routes/users");
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+// break stuff for testing
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "pug");
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(logger("dev"));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
@@ -29,8 +30,9 @@ app.use(
   })
 );
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+// break some more stuff
+// app.use("/", indexRouter);
+// app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
